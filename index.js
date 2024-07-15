a5d3a4585c678328f5a6424a33fde484e2d24157
shumaila-39 <159179205+shumaila-39@users.noreply.github.com>
2024-07-15 17:28:03 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+#! usr/bin/env node
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const inquirer_1 = __importDefault(require("inquirer"));
+const answer = await inquirer_1.default.prompt([{ message: "enter your first number", type: "number", name: "firstNumber" },
+    { message: "enter your second number", type: "number", name: "secondNumber" },
+    { message: "select one of operator to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "subtraction", "multiplication", "division"],
+    },
+]);
+//conditional statement    
+if (answer.operator === "addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    "please select correct operator";
+}
